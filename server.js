c2ae59e32ce0d121975286bb776e4a11d373ee25
souzaiosoficial <Souzathiarles93@gmail.com>
2026-04-12 18:36:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const DB_FILE = path.join(__dirname, 'keys.json');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("public"));
 
 // Load/save keys
